e53d02ecf8ddd83bcd1c592191c261a40f047ba9
Hassan Ali Khan <hassan.ali@musewerx.com>
2014-02-14 02:41:38 -0800

    signupcommit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SignUpWizard::Application.config.secret_key_base = '467959ebad3f822f1f3faafc65f56370529666dc42278497182749c2315d3a43a62db7c5ebb74914c9650bf1ff0aab4295aebd55396afd3a690f36759d1194f7'
